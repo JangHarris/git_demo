41d80facf7052cafde4abdb1298b29047c784b1c
jang <17379742290@163.com>
2019-05-15 19:36:58 +0800

    第一次提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @user lenovo
+ * @date 2019/5/15 17:01
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
